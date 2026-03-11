e929b6b1ee9a9a635accdbc10567cc1a6769c0dd
Adrito Rafsan <116940100+adritorafsan@users.noreply.github.com>
2026-03-11 06:35:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="h-screen w-full bg-[#F5F5F5] grid grid-cols-2 ">
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
